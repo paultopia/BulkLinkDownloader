531bf38efefeb307e2710f7010ec3f1a5fad7db9
Paul Gowder <paultopia@users.noreply.github.com>
2019-03-19 09:51:56 -0500

    add extension tester

--- Sources/BulkLinkDownloader/main.swift
+++ Sources/BulkLinkDownloader/main.swift
@@ -5,8 +5,16 @@ let bg = BackgroundDownloader()
 
 let url = URL(string: "http://paultopia.org/downloadtest.html")!
 
+func exten(ofLink link: String, isIn extensList: [String]) -> Bool {
+    if let xtn = URL(string: link)?.pathExtension {
+        return extensList.contains(xtn)
+    }
+    return false
+}
+
 func listLinks(_ html: String) -> [String] {
     let doc = try! SwiftSoup.parse(html)
+    // note to self: need to use baseUri argument with original url to get relative links sorted.
     let links = try! doc.select("a")
     let out = links.array().map {try! $0.attr("href")}
     return out
