561655d0358f8f10b288ee35082b53326f8be683
Paul Gowder <paul.gowder@gmail.com>
2019-03-19 15:15:59 -0500

    cleanup code a little, but need to test on big download files

--- Sources/BulkLinkDownloader/main.swift
+++ Sources/BulkLinkDownloader/main.swift
@@ -3,7 +3,7 @@ import SwiftSoup
 
 let bg = BackgroundDownloader()
 
-let url = URL(string: "http://paultopia.org/downloadtest.html")!
+let address = "http://paultopia.org/downloadtest.html"
 
 func exten(ofLink link: String, isIn extensList: [String]) -> Bool {
     if let xtn = URL(string: link)?.pathExtension {
@@ -12,24 +12,27 @@ func exten(ofLink link: String, isIn extensList: [String]) -> Bool {
     return false
 }
 
-func listLinks(_ html: String) -> [String] {
-    let doc = try! SwiftSoup.parse(html)
-    // note to self: need to use baseUri argument with original url to get relative links sorted.
+func listLinks(_ html: String, origin: String) -> [String] {
+    let doc = try! SwiftSoup.parse(html, origin)
     let links = try! doc.select("a")
-    let out = links.array().map {try! $0.attr("href")}
+    let out = links.array().map {try! $0.absUrl("href")}
     return out
 }
 
-func printLinks(_ s: String){
-    let links = listLinks(s)
-    var link: String
+func printLinks(_ s: String, origin: String){
+    let links = listLinks(s, origin: origin)
     for l in links {
-        link = "http://paultopia.org/" + l
-        print(link)
-        bg.addDownloadToQueue(address: link)
+        print(l)
+        bg.addDownloadToQueue(address: l)
     }
     bg.runAllDownloads()
 }
 
-let data = try! String(contentsOf: url)
-printLinks(data)
+func scrape_data(from address: String){
+    let url = URL(string: address)!
+    let data = try! String(contentsOf: url)
+    printLinks(data, origin: address)
+}
+
+
+scrape_data(from: address)
